de47dd7c3f533f11574a7fcd7826e285233b079b
Антон Гаврилов <gavor0120@gmail.com>
2025-03-28 21:43:08 +0100

    fix font issues on different OS

--- src/Profitocracy.Mobile/MauiProgram.cs
+++ src/Profitocracy.Mobile/MauiProgram.cs
@@ -30,7 +30,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-				fonts.AddFont("Ionicons.ttf");
+				fonts.AddFont("Ionicons.ttf", "Ionicons");
 			})
 			.RegisterAppServices()
 			.RegisterViewModels()
