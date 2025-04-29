0fb89660004c85ad0cd5f4ff49d1e2750d26ac89
Anton Gavrilov <gavor0120@gmail.com>
2025-04-29 19:23:33 +0200

    styles and localization updates

--- src/Profitocracy.Mobile/Resources/Strings/AppResources.es.Designer.cs
+++ src/Profitocracy.Mobile/Resources/Strings/AppResources.es.Designer.cs
@@ -11,24 +11,24 @@ namespace Profitocracy.Mobile.Resources.Strings {
     using System;
     
     
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class AppResources {
+    internal class AppResources_es {
         
         private static System.Resources.ResourceManager resourceMan;
         
         private static System.Globalization.CultureInfo resourceCulture;
         
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal AppResources() {
+        internal AppResources_es() {
         }
         
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.Equals(null, resourceMan)) {
-                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("Profitocracy.Mobile.Resources.Strings.AppResources", typeof(AppResources).Assembly);
+                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("Profitocracy.Mobile.Resources.Strings.AppResources_es", typeof(AppResources_es).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
