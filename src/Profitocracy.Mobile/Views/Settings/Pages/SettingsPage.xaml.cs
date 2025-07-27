5b4f0afc9bd63267adc75d56fb530ebe0b1cd4c4
Anton Gavrilov <gavor0120@gmail.com>
2025-07-27 15:48:09 +0200

    hotfixed app rating

--- src/Profitocracy.Mobile/Views/Settings/Pages/SettingsPage.xaml.cs
+++ src/Profitocracy.Mobile/Views/Settings/Pages/SettingsPage.xaml.cs
@@ -40,31 +40,7 @@ public partial class SettingsPage : BaseContentPage
 
     private void RateAppButton_OnClicked(object? sender, EventArgs e)
     {
-        ProcessAction(async () =>
-        {
-            try
-            {
-                await _appRating.PerformInAppRateAsync();
-                return;
-            }
-            catch (Exception)
-            {
-                // Ignore
-            }
-
-            try
-            {
-                await _appRating.PerformRatingOnStoreAsync();
-                return;
-            }
-            catch (Exception)
-            {
-                // Ignore
-            }
-
-            // At least open the app page in a browser
-            await Browser.Default.OpenAsync(UrlConstants.StoreUrl, BrowserLaunchMode.SystemPreferred);
-        });
+        ProcessAction(RateAppAsync);
     }
 
     private async Task NavigateToPage<T>() where T : Page
@@ -76,4 +52,63 @@ public partial class SettingsPage : BaseContentPage
             await Navigation.PushAsync(page);
         }
     }
+
+#if ANDROID
+    private async Task RateAppAsync()
+    {
+        try
+        {
+            await _appRating.PerformRatingOnStoreAsync(UrlConstants.ApplicationId);
+            return;
+        }
+        catch (Exception)
+        {
+            // Ignore
+        }
+
+        try
+        {
+            await _appRating.PerformInAppRateAsync();
+            return;
+        }
+        catch (Exception)
+        {
+            // Ignore
+        }
+
+        // At least open the app page in a browser
+        await Browser.Default.OpenAsync(UrlConstants.StoreUrl, BrowserLaunchMode.SystemPreferred);
+    }
+#elif IOS
+    private async Task RateAppAsync()
+    {
+        try
+        {
+            await _appRating.PerformInAppRateAsync();
+            return;
+        }
+        catch (Exception)
+        {
+            // Ignore
+        }
+
+        try
+        {
+            await _appRating.PerformRatingOnStoreAsync(UrlConstants.ApplicationId);
+            return;
+        }
+        catch (Exception)
+        {
+            // Ignore
+        }
+
+        // At least open the app page in a browser
+        await Browser.Default.OpenAsync(UrlConstants.StoreUrl, BrowserLaunchMode.SystemPreferred);
+    }
+#else
+    private async Task RateAppAsync()
+    {
+        await Browser.Default.OpenAsync(UrlConstants.StoreUrl, BrowserLaunchMode.SystemPreferred);
+    }
+#endif
 }
