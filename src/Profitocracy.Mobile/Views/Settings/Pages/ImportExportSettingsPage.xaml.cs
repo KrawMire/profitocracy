1925df73a6c02095e3f284345ca96347dfb67326
Anton Gavrilov <gavor0120@gmail.com>
2025-07-29 18:21:41 +0200

    auth page style improvement. backup settings page displays after performed actions

--- src/Profitocracy.Mobile/Views/Settings/Pages/ImportExportSettingsPage.xaml.cs
+++ src/Profitocracy.Mobile/Views/Settings/Pages/ImportExportSettingsPage.xaml.cs
@@ -1,3 +1,4 @@
+using Profitocracy.Mobile.Resources.Strings;
 using Profitocracy.Mobile.ViewModels.Settings;
 
 namespace Profitocracy.Mobile.Views.Settings.Pages;
@@ -13,8 +14,22 @@ public partial class ImportExportSettingsPage
     }
 
     private void ImportButton_OnClicked(object? sender, EventArgs e)
-        => ProcessAction(_viewModel.ImportAsync);
+        => ProcessAction(async () =>
+        {
+            await _viewModel.ImportAsync();
+            await DisplayAlert(
+                AppResources.ImportExport_SuccessImportAlert_Title,
+                AppResources.ImportExport_SuccessImportAlert_Description,
+                AppResources.ImportExport_SuccessImportAlert_Ok);
+        });
 
     private void ExportButton_OnClicked(object? sender, EventArgs e)
-        => ProcessAction(_viewModel.ExportAsync);
+        => ProcessAction(async () =>
+        {
+            await _viewModel.ExportAsync();
+            await DisplayAlert(
+                AppResources.ImportExport_SuccessExportAlert_Title,
+                AppResources.ImportExport_SuccessExportAlert_Description,
+                AppResources.ImportExport_SuccessExportAlert_Ok);
+        });
 }
