af81cd5ca2dd3fcdeba5a6fb6d5aa515e723ece3
Anton Gavrilov <gavor0120@gmail.com>
2025-08-03 18:55:51 +0200

    fixed specifying time of transactions of savings type

--- src/Profitocracy.Mobile/ViewModels/Transactions/EditTransactionPageViewModel.cs
+++ src/Profitocracy.Mobile/ViewModels/Transactions/EditTransactionPageViewModel.cs
@@ -48,7 +48,7 @@ public class EditTransactionPageViewModel : BaseNotifyObject
 
     private bool _isMultiCurrency;
     private RecurringTransactionIntervalModel? _selectedInterval;
-    
+
     public EditTransactionPageViewModel(
         IProfileRepository profileRepository,
         ITransactionRepository transactionRepository,
@@ -370,7 +370,7 @@ public class EditTransactionPageViewModel : BaseNotifyObject
         {
             category = new TransactionCategory((Guid)Category.Id)
             {
-                Name = Category.Name
+                Name = Category.Name,
             };
         }
 
@@ -378,7 +378,7 @@ public class EditTransactionPageViewModel : BaseNotifyObject
 
         if (SelectedInterval is not null)
         {
-            recurringTransactionInfo = new RecurringTransactionInfo() 
+            recurringTransactionInfo = new RecurringTransactionInfo
             {
                 Interval = (RecurringTransactionInterval)SelectedInterval.Value
             };
@@ -424,8 +424,10 @@ public class EditTransactionPageViewModel : BaseNotifyObject
             // Transaction type is Expense, Multi-currency and spending
             // type is saved, so it is saving funds in another currency
             1 when _spendingType == 2 => TransactionDestination.SavingsBalance,
-            _ => TransactionDestination.Expense
+            _ => TransactionDestination.Expense,
         };
+
+        var transactionTimestamp = _timestamp.Date.Add(_time);
 
         return TransactionFactory.CreateMultiCurrencyTransaction(
             id: transactionId,
@@ -437,7 +439,7 @@ public class EditTransactionPageViewModel : BaseNotifyObject
             (TransactionType)_transactionType,
             _spendingType is null or -1 ? null : (SpendingType)_spendingType,
             destination,
-            _timestamp,
+            transactionTimestamp,
             _description,
             geoTag: null,
             category,
